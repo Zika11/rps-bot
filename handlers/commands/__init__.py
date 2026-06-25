58235aaec826703b26ab49278e60acd6168c96a1
Zika11 <86346072+Zika11@users.noreply.github.com>
2026-06-25 15:13:16 +0300

    Create __init__.py

---
+++ handlers/commands/__init__.py
@@ -0,0 +1,5 @@
+from .user_commands import start, me_command, daily_command, referral_command
+from .game_commands import game_command, web_command
+from .shop_commands import shop_command, buy_command, market_sell_command, battlepass_command
+from .admin_commands import admin_panel, start_channel_command, stop_channel_command
+from .group_commands import massbattle_command, teambattle_command, drop_command
