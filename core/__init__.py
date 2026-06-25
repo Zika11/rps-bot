1dc76bae9117fcd6e8e24ac3465580aff034e892
Zika11 <86346072+Zika11@users.noreply.github.com>
2026-06-25 15:34:47 +0300

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -2,3 +2,4 @@ from .game_engine import get_result, bot_choice, calculate_winner
 from .tournament_manager import *
 from .social_manager import *
 from .misc_manager import *
+from .channel_manager import channel_voting_loop
