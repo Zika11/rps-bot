b7367384442f273361651395034977f6e1280cfc
Zika11 <86346072+Zika11@users.noreply.github.com>
2026-06-25 15:01:58 +0300

    Update __init__.py

--- handlers/callbacks/__init__.py
+++ handlers/callbacks/__init__.py
@@ -1,1 +1,6 @@
-from . import navigation, game, channel, shop, social, admin
+from .navigation import handle as navigation_handler
+from .game import handle as game_handler
+from .channel import handle as channel_handler
+from .shop import handle as shop_handler
+from .social import handle as social_handler
+from .admin import handle as admin_handler
