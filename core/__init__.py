7c1397e0129acaa909102017041aeff5ec9f4f2c
Zika11 <86346072+Zika11@users.noreply.github.com>
2026-06-26 12:26:51 +0300

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,9 +1,6 @@
 # core/__init__.py
 from .game_engine import get_result, bot_choice, calculate_winner
-from .tournament_manager import *
-from .social_manager import *
-from .misc_manager import *
 from .channel_manager import channel_voting_loop
 from .shop_manager import *
-from .redis_client import RedisClient, redis_client
-from .google_sheets import GoogleSheetsClient, gsheets
+from .social_manager import *
+from .misc_manager import *
